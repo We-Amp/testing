0b23e2775001f1f27766d94f02dddab51128fbd2
Piyush Deshpande <piyush.d@gdbtech.in>
2017-12-07 18:22:45 +0530

    WIP- Update test file to include content length in data

--- prototesting/fcgi/server.py
+++ prototesting/fcgi/server.py
@@ -46,6 +46,7 @@ class Server(EventProcessor):
         self.address = "0.0.0.0"
         self.listen_thread = None
         self.tcpsock = {}
+        self.keepconn = False
 
     def config(self, config):
         """
@@ -108,26 +109,6 @@ class Server(EventProcessor):
                 return
 
             self.handle_events(address, data)
-            # logging.info(data.decode())
-            # logging.info(len(data))
-            # logging.info(fcgi.FCGI_HEADER_LEN)
-            # fcgi_header = data[:fcgi.FCGI_HEADER_LEN]
-            # version, fcgi_type, request_id, content_length, padding_length = struct.unpack(fcgi.FCGI_Header, fcgi_header)
-            # logging.info("fcgi_type" + str(fcgi_type))
-            # logging.info("content_length" + str(content_length))
-            # logging.info("padding_length" + str(padding_length))
-            # content_data = data[fcgi.FCGI_HEADER_LEN:content_length]
-            # pos = 0
-            # decoded_data = {}
-            # while pos < len(content_data):
-            #     pos, (name, value) = fcgi.decode_pair(content_data, pos)
-            #     if name:
-            #         logging.info("Name is " + name.decode())
-            #         logging.info("Value is " + value.decode())
-            #         logging.info("Pos is " + str(pos))
-            #         # decoded_data[name.decode()] = value.decode()
-
-            # logging.info(decoded_data)
 
     def handle_events(self, address, data):
         length = len(data)
@@ -147,9 +128,16 @@ class Server(EventProcessor):
                                     lambda event, data: True, None)
                 logging.info("FCGI_GET_VALUES")
             elif fcgi_type == fcgi.FCGI_BEGIN_REQUEST:
+
                 self.event_received("FCGI_BEGIN_REQUEST",
                                     Response(self, address, content_data),
                                     lambda event, data: True, None)
+                unused_role, flags = struct.unpack(fcgi.FCGI_BeginRequestBody,
+                                                   content_data)
+
+                if flags & 1:
+                    self.keepconn = True
+
                 logging.info("FCGI_BEGIN_REQUEST")
             elif fcgi_type == fcgi.FCGI_ABORT_REQUEST:
                 self.event_received("FCGI_ABORT_REQUEST",
@@ -162,32 +150,34 @@ class Server(EventProcessor):
                 decoded_data = {}
                 while pos < len(content_data):
                     pos, (name, value) = fcgi.decode_pair(content_data, pos)
-                    # if name:
-                    #     logging.info("Name is " + name.decode())
-                    #     logging.info("Value is " + value.decode())
-                    #     logging.info("Pos is " + str(pos))
                     decoded_data[name.decode()] = value.decode()
 
+                logging.info(decoded_data)
                 self.event_received("FCGI_PARAMS",
                                     Response(self, address, decoded_data),
                                     lambda event, data: True, None)
-                # if not decoded_data:
-                # self.send_record(
-                #     address,
-                #     type="FCGI_STDOUT",
-                #     data="Content-type: text/html\r\n\r\nHello",
-                #     content_length=len(
-                #         "Content-type: text/html\r\n\r\nHello"))
 
-                # self.send_record(
-                #     type="FCGI_STDOUT", data="", content_length=len(""))
-
-                # self.send_record(address)
             elif fcgi_type == fcgi.FCGI_STDIN:
+                logging.info("FCGI_STDIN")
                 self.event_received("FCGI_STDIN",
                                     Response(self, address, content_data),
                                     lambda event, data: True, None)
-                logging.info("FCGI_STDIN")
+
+                if __name__ == "__main__":
+                    if not content_data:
+                        self.send_record(
+                            address,
+                            type="FCGI_STDOUT",
+                            data="Content-type: text/html\r\n\r\nYeello1\r\n")
+                        self.send_record(
+                            address, type="FCGI_STDOUT", data="Yeello2\r\n")
+                        self.send_record(
+                            address, type="FCGI_STDOUT", data="Yeello3\r\n")
+
+                        self.send_record(
+                            address, type="FCGI_STDOUT", data="\r\n\r\n")
+                        self.send_record(address)
+
             elif fcgi_type == fcgi.FCGI_DATA:
                 self.event_received("FCGI_DATA",
                                     Response(self, address, content_data),
@@ -230,21 +220,23 @@ class Server(EventProcessor):
             data = struct.pack(fcgi.FCGI_EndRequestBody, 0, 0)
             content_length = fcgi.FCGI_EndRequestBody_LEN
 
+        if data and not content_length:
+            content_length = len(data)
+
         version = int(version)
         content_length = int(content_length)
 
         # logging.info("Data: " + data)
         # logging.info("contentLength: " + str(content_length))
+
         header = struct.pack(fcgi.FCGI_Header, version, type, request_id,
                              content_length, padding_length)
 
+        logging.info("Sending Record " + str(type) + " " + str(content_length))
         tcpconn.sendall(header)
 
         if hasattr(data, "encode"):
             data = data.encode()
-
-        if data and not content_length:
-            content_length = len(data)
 
         if content_length:
             tcpconn.sendall(data)
@@ -252,7 +244,9 @@ class Server(EventProcessor):
         if padding_length:
             tcpconn.sendall(b'\x00' * padding_length)
 
-        if type == fcgi.FCGI_END_REQUEST:
+        if type == fcgi.FCGI_END_REQUEST and not self.keepconn:
+            logging.info("Closing tcp conn")
+            tcpconn.shutdown(socket.SHUT_RDWR)
             tcpconn.close()
 
     def kill(self):
