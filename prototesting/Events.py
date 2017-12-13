f2b465d24f543801324867be19b41bfd946d01df
Piyush Deshpande <piyush.d@gdbtech.in>
2017-12-13 14:40:48 +0530

    Update launcher to wait for keyword in its output

--- prototesting/Events.py
+++ prototesting/Events.py
@@ -1,9 +1,15 @@
 """
-Class which want to register for events or post events needs to derive from this class.
+Basic event handlers
 """
+
+# :TODO(Piyush): rename this to event.py
 
 
 class EventProcessor:
+    """
+    Class which want to register for events or post events needs to derive from this class.
+    """
+
     def __init__(self, context):
         self.context = context
 
@@ -17,3 +23,10 @@ class EventProcessor:
                      in test.
         """
         self.context.event_received(event_name, response, compare_func, event)
+
+    def register_event(self, waitfor_event_name, wait_event, name, data):
+        """
+        Subclass should call this function when it wants to register custom event.
+        It can be used to handle internal events without explicitly adding them to the test json
+        """
+        self.context.register_event(waitfor_event_name, wait_event, name, data)
--- prototesting/externallauncher/launcher.py
+++ prototesting/externallauncher/launcher.py
@@ -2,8 +2,10 @@
 Launch external process with configutation
 """
 
+import io
 import logging
 import subprocess
+import threading
 from os.path import join
 
 from Events import EventProcessor
@@ -17,70 +19,106 @@ class Launcher(EventProcessor):
     def __init__(self, context=None):
         EventProcessor.__init__(self, context)
         self.output = None
-        self.args_list = []
-        self.root_access = False
-        self.password = None
-        self.cmd = None
+        self.buffer = io.StringIO()
 
-    def launch(self,
-               cmd,
-               arguments=None,
-               path=None,
-               root_access=False,
-               password=None):
+    def launch(self, cmd, arguments=None, path=None, root_access=False):
         """
         Launch the current command and store its output
         """
         # Create args list for subprocess run
-        self.root_access = root_access
-        if self.root_access:
-            self.password = password
+        args_list = []
+        if root_access:
             if "sudo" not in cmd:
-                self.args_list.append("sudo")
+                args_list.append("sudo")
 
         if path:
-            self.cmd = join(path, cmd)
-            logging.info(self.cmd)
-        else:
-            self.cmd = cmd
+            cmd = join(path, cmd)
+            logging.debug(cmd)
 
-        self.args_list.append(self.cmd)
+        args_list.append(cmd)
 
         # Config should be list of relvant switches and options
         if arguments:
-            self.args_list += arguments
+            args_list += arguments
 
-        logging.info(self.args_list)
+        logging.debug(args_list)
 
-        if self.password:
-            self.output = subprocess.Popen(
-                self.args_list, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-
-        else:
-            self.output = subprocess.Popen(
-                self.args_list, stdout=subprocess.PIPE)
-
-        if self.root_access and self.password:
-            self.output.communicate(self.password.encode())
-
+        self.output = subprocess.Popen(
+            args_list,
+            close_fds=True,
+            stdout=subprocess.PIPE,
+        )
+        self.get_output()
         # :TODO(Piyush) Handle wait for here based on output status and data
         self.event_received("ProcessStarted", self, lambda event, data: True,
                             None)
 
         return self
 
+    def read_output(self, pipe, funcs):
+        """
+        Iterate over stdout
+        """
+        for line in iter(pipe.readline, b''):
+            funcs(line.decode())
+        pipe.close()
+
     def get_output(self):
         """
-        Returns the output stored for given command
+        Read Output stream as the output is generated on stdout
         """
-        out, unused_err = self.output.communicate(timeout=1)
-        print(out.decode())
+        stdout_thread = threading.Thread(
+            target=self.read_output,
+            args=(self.output.stdout, self.buffer.write))
+
+        # python process won't hang if the main thread is exited
+        # thread dies with the program
+        stdout_thread.daemon = True
+        stdout_thread.start()
+
+        self.output.wait()
+
+        stdout_thread.join()
+
+    def wait_for_keyword(self, keyword):
+        """
+        Check if required keyword is found in buffer
+        """
+        while True:
+            if keyword in self.buffer.getvalue():
+                logging.debug("found: " + keyword)
+                self.buffer = io.StringIO()
+                self.event_received("_ScanFor", keyword,
+                                    lambda event, data: True, None)
+                return
+
+    def scan_for(self, keyword=None):
+        """
+        Scan command output for particular string
+        """
+        # Create a new event and pass it to a new thread to wait for required
+        # output or wait for timeout
+
+        # :TODO(Piyush): Move time out to global config
+        timeout = 20
+        wait_for_event = threading.Event()
+        self.register_event("_ScanFor", wait_for_event, "_ScanFor", keyword)
+
+        wait_for_thread = threading.Thread(
+            target=self.wait_for_keyword, args=(keyword, ))
+
+        wait_for_thread.start()
+        success = wait_for_event.wait(timeout=timeout)
+        if not success:
+            logging.error("Event Timeout, Not found keyword : " + keyword)
+            logging.error(self.buffer.getvalue())
 
     def kill(self):
         """
         Kill the open process
         """
         self.output.kill()
+        self.buffer.close()
 
 
 def create(context=None):
